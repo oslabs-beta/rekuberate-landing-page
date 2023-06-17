b88504bafea5d0d0e61b6f57b674d44a751da213
Kevin Fan <kevinlifan@yahoo.com>
2023-06-17 12:17:13 -0400

    footer styling

--- frontend/components/NavBar/Footer.js
+++ frontend/components/NavBar/Footer.js
@@ -5,7 +5,31 @@ export default function Footer() {
   return (
     <div className='footer'>
       <p className='footerText'>
-        Built with <p id='heart'>♥</p> by Hunter, Kai, Thad, Fabrizzio, and Kevin.
+        Built with{' '}
+        <p id='heart' className='bottomNames'>
+          ♥
+        </p>{' '}
+        by{' '}
+        <p id='hunter' className='bottomNames'>
+          Hunter
+        </p>
+        ,{' '}
+        <p id='kai' className='bottomNames'>
+          Kai
+        </p>
+        ,{' '}
+        <p id='thad' className='bottomNames'>
+          Thad
+        </p>
+        ,{' '}
+        <p id='fab' className='bottomNames'>
+          Fabrizzio
+        </p>
+        , and{' '}
+        <p id='kevin' className='bottomNames'>
+          Kevin
+        </p>
+        .
       </p>
       <p className='footerText'>© 2023</p>
     </div>

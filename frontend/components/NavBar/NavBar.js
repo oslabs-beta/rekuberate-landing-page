294495684cf9fdd965229890e8f1f768705cff18
Kevin Fan <kevinlifan@yahoo.com>
2023-06-17 15:00:31 -0400

    added dark mode

--- frontend/components/NavBar/NavBar.js
+++ frontend/components/NavBar/NavBar.js
@@ -1,31 +1,58 @@
-import React from "react";
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import "../CSS/Nav.css";
-import wheel from "./pictures/wheel.png";
-
+import '../CSS/Nav.css';
+import wheel from './pictures/wheel.png';
 
 export default function NavBar() {
   const navigate = useNavigate();
+
+  const [colorTheme, setColorTheme] = useState('');
+
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches && colorTheme === null) {
+    setColorTheme('dark');
+  }
+
+  document.querySelector('body').setAttribute('theme', colorTheme);
+
   return (
     <>
-      <nav className="navbar">
+      <nav className='navbar'>
         <div>
           <button onClick={() => navigate('/')}>
-            <img src={wheel} alt="logo" className="nav-logo"/>
+            <img src={wheel} alt='logo' className='nav-logo' />
           </button>
         </div>
-        <div className="nav_actions">
-          <button className="nav_links" onClick={() => navigate('/docs')}> Docs </button>
-          <button className="nav_links" onClick={() => navigate('/about')}> About Us </button>
-          <a
-            className="nav_link nav_links"
-            href="https://medium.com/the-programmer/kubernetes-fundamentals-for-absolute-beginners-architecture-components-1f7cda8ea536"
-          >
+        <div className='nav_actions'>
+          <button className='nav_links' onClick={() => navigate('/docs')}>
+            Docs
+          </button>
+          <button className='nav_links' onClick={() => navigate('/about')}>
+            About Us
+          </button>
+          <button
+            className='nav_links'
+            onClick={(e) => {
+              e.preventDefault();
+              window.location.href =
+                'https://medium.com/the-programmer/kubernetes-fundamentals-for-absolute-beginners-architecture-components-1f7cda8ea536';
+            }}>
             Blog
-          </a>
-          <a className="nav_link nav_links" href="https://github.com/oslabs-beta/ReKuberate">
+          </button>
+          <button
+            className='nav_links'
+            onClick={(e) => {
+              e.preventDefault();
+              window.location.href = 'https://github.com/oslabs-beta/ReKuberate';
+            }}>
             Github
-          </a>
+          </button>
+          <button
+            className='nav_links'
+            onClick={() => {
+              colorTheme === 'dark' ? setColorTheme('light') : setColorTheme('dark');
+            }}>
+            <i class='fa-solid fa-moon'></i>
+          </button>
         </div>
       </nav>
     </>

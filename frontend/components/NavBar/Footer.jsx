a63bc9ad45c2efc0129576a8fb92984120b92e1a
Kevin Fan <kevinlifan@yahoo.com>
2023-06-21 15:22:35 -0400

    added social media links

--- frontend/components/NavBar/Footer.jsx
+++ frontend/components/NavBar/Footer.jsx
@@ -4,6 +4,17 @@ import '../CSS/footer.css';
 export default function Footer() {
   return (
     <div className='footer'>
+      <p className='footerLinks'>
+        <a style={{ color: 'inherit' }} href='https://www.linkedin.com/company/rekuberate-open-source/'>
+          <i className='fa-brands fa-linkedin'></i>
+        </a>{' '}
+        <a style={{ color: 'inherit' }} href='https://twitter.com/ReKuberateOS'>
+          <i className='fa-brands fa-twitter'></i>
+        </a>{' '}
+        <a style={{ color: 'inherit' }} href='https://github.com/oslabs-beta/ReKuberate'>
+          <i class='fa-brands fa-github'></i>
+        </a>
+      </p>
       <p className='footerText'>
         Built with{' '}
         <p id='heart' className='bottomNames'>
@@ -28,10 +39,9 @@ export default function Footer() {
         , and{' '}
         <p id='kevin' className='bottomNames'>
           Kevin
-        </p>
-        .
+        </p>{' '}
+        © 2023
       </p>
-      <p className='footerText'>© 2023</p>
     </div>
   );
 }
